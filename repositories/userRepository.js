f499bf97423bf1df6acba7e88b0a08094a89cb48
nuno <nunopunkas@gmail.com>
2025-07-18 21:06:27 +0200

    added users.json

--- repositories/userRepository.js
+++ repositories/userRepository.js
@@ -1,10 +1,23 @@
 // repositories/userRepository.js
-
 const fs = require("fs");
 const path = require("path");
 const usersFile = path.join(__dirname, "users.json");
 
+function loadUsersFromFile() {
+  try {
+    const data = fs.readFileSync(usersFile);
+    return JSON.parse(data);
+  } catch (err) {
+    fs.writeFileSync(usersFile, "[]");
+    return [];
+  }
+}
 
+function saveUsersToFile(users) {
+  fs.writeFileSync(usersFile, JSON.stringify(users, null, 2));
+}
+
+let users = loadUsersFromFile();
 
 function findUserByUsername(username) {
   return users.find(user => user.username === username);
@@ -12,7 +25,16 @@ function findUserByUsername(username) {
 
 function createUser(user) {
   users.push(user);
+  saveUsersToFile(users);
   return user;
+}
+
+function updateUserProgress(username, currentRoom) {
+  const user = findUserByUsername(username);
+  if(user) {
+    user.currentRoom = currentRoom;
+    saveUsersToFile(users);
+  }
 }
 
 function getAllUsers() {
@@ -23,5 +45,6 @@ module.exports = {
   users,
   findUserByUsername,
   createUser,
+  updateUserProgress,
   getAllUsers,
 };
